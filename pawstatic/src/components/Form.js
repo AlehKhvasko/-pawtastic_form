6cb590d1dd73d9e2af041ff41d7533a8b08308f5
Student_MA122 <student_ma122@Student-MA122.fios-router.home>
2020-03-29 20:45:03 -0400

    centered form

--- pawstatic/src/components/Form.js
+++ pawstatic/src/components/Form.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
+import './Form.css';
 
 export default class App extends Component {
     render() {
         return(
-            <div>
-    <div className='pawstatic'>
-        <div className= 'radioButtons'>
-            <span>Human profile</span>
-            <span>Pet basics</span>
-            <span>Pet details</span>
-            <span>Confirm</span>
+    <div>
+        <div className='static'>
+            <div className= 'radioButtons'>
+                <span>Human profile</span>
+                <span>Pet basics</span>
+                <span>Pet details</span>
+                <span>Confirm</span>
+            </div>
         </div>
     </div>
-            </div>
         )
     }
 }
