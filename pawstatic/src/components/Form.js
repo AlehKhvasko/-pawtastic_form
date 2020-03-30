ebed1037d2f9a94a5722c57e8b0d95a9fc6249c1
Student_MA122 <student_ma122@Student-MA122.fios-router.home>
2020-03-30 01:18:35 -0400

    added img

--- pawstatic/src/components/Form.js
+++ pawstatic/src/components/Form.js
@@ -10,10 +10,19 @@ export default class App extends Component {
             <div className= 'radioButtons'>
                 <div className='radioButtons_icon'>Pawstatic</div>
                 <div className='radioButtons_btn'>
-                    <span>Human profile</span>
-                    <span>Pet basics</span>  
-                    <span>Pet details</span>
-                    <span>Confirm</span>
+                <span>
+                    <input type="checkbox" id="toggle" class="checkbox" />
+                    <label for="toggle" class="switch">
+                    </label>Human profile</span><div className="vertical"></div> 
+                    <span><input type="checkbox" id="toggle" class="checkbox" />
+                    <label for="toggle" class="switch">
+                    </label>Pet basics</span><div className="vertical"></div>  
+                    <span><input type="checkbox" id="toggle" class="checkbox" />
+                    <label for="toggle" class="switch">
+                    </label>Pet details</span><div className="vertical"></div> 
+                    <span><input type="checkbox" id="toggle" class="checkbox" />
+                    <label for="toggle" class="switch">
+                    </label>Confirm</span>
                 </div>
                 <div className='radioButtons_image'>
                      <span>Save and Exit<hr/></span>
@@ -26,7 +35,6 @@ export default class App extends Component {
                     <div className="dog_form__top">
                         <Input name="Name" type="text" value="Pet's name"></Input>
                         <div className="image_load">
-                            <div className="image_load"></div>
                             <p>Upload a photo</p>
                         </div>
                     </div>
@@ -39,7 +47,7 @@ export default class App extends Component {
                         <Input name="Breed" type="text" value="Pet's breed"></Input>
                     </div>
                     <div className="dog_form__weight">
-                        <Input name="Weight" type="radio" value="0-100lbs"></Input>
+                        <Input name="Weight" type="radio" value="0-25"></Input>
                     </div>
                 </div>
                 <div className="dog_form__footer">
